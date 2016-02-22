25643d2ffc65b72d3f61cd01179ce25c248cfc83
mastinux <andrea.pantaleo.93@gmail.com>
2016-02-22 01:58:47 +0100

    checked first html, without positive results

--- XOrueTeb/models.py
+++ XOrueTeb/models.py
@@ -5,26 +5,32 @@ import sys
 # https://docs.python.org/2/library/htmlparser.html
 # https://docs.python.org/2/howto/urllib2.html
 
-ORUE_TEB = "'http://web.eurobet.it/webeb/scommesse-sportive'"
+ORUE_TEB = "http://web.eurobet.it/webeb/scommesse-sportive"
+
+# todo : html does't contain proper values, check next site
 
 
 class OrueTebParser(HTMLParser):
 
     def handle_starttag(self, tag, attrs):
-        print "Encountered a start tag: <", tag, ">"
+        if tag == "div":
+            #print "\n"
+            for name, value in attrs:
+                print value
+                if name == 'class' and value == 'box_container_scommesse_quote':
+                    print value
 
-    def handle_endtag(self, tag):
-        print "Encountered an end tag : </", tag, ">"
+    #def handle_endtag(self, tag):
+    #    print "Encountered an end tag : </", tag, ">"
 
-    def handle_data(self, data):
-        print "Encountered some data  :", data
+    #def handle_data(self, data):
+    #    print "data :\t\t", data
 
 
 req = Request(ORUE_TEB)
 response = urlopen(req)
-html = response.read()
-print html
-#html_encoded = html.decode('utf-8')
+encoding = response.headers.getparam('charset')
+html = response.read().decode(encoding)
 
-#parser = OrueTebParser()
-#parser.feed(html)
+parser = OrueTebParser()
+parser.feed(html)
